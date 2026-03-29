45f366b77811d5f523ef0c3ceaefc919f49f9ba4
EliClay1 <eliclay1@byu.edu>
2026-03-29 17:39:37 -0600

    Explosive trap explodes and has animation.

--- src/main/java/com/displace/cavetraps/block/ExplosiveTrapBlock.java
+++ src/main/java/com/displace/cavetraps/block/ExplosiveTrapBlock.java
@@ -1,20 +1,49 @@
 package com.displace.cavetraps.block;
 
+import com.displace.cavetraps.blockentities.ExplosiveTrapBlockEntity;
 import com.displace.cavetraps.blockentities.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jspecify.annotations.Nullable;
 
 public class ExplosiveTrapBlock extends Block implements EntityBlock {
     public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
+    public static final Property<Boolean> TRIGGERED = BooleanProperty.create("triggered");
+    private static final VoxelShape SHAPE = Block.box(1, 0, 1, 15, 20, 15);
+
     public ExplosiveTrapBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH)
+                .setValue(TRIGGERED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, TRIGGERED);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 
     @Override
@@ -39,7 +68,39 @@ public class ExplosiveTrapBlock extends Block implements EntityBlock {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (!level.isClientSide()) {
+            activate(level, pos, state);
+        }
+        return super.useWithoutItem(state, level, pos, player, hitResult);
+    }
+
+//    @Override
+//    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+//        if (!level.isClientSide()) {
+//            activate(level, pos, state);
+//        }
+//        super.stepOn(level, pos, state, entity);
+//    }
+
+    private void activate(Level level, BlockPos pos, BlockState state) {
+        if (state.getValue(TRIGGERED)) {
+            return;
+        }
+        level.setBlock(pos, state.setValue(TRIGGERED, true), 3);
+        if (level.getBlockEntity(pos) instanceof ExplosiveTrapBlockEntity trap) {
+            trap.activate();
+        }
+        level.scheduleTick(pos, this, 60);
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (!state.getValue(TRIGGERED)) {
+            return;
+        }
+        level.removeBlock(pos, false);
+        level.explode(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 4.0F, Level.ExplosionInteraction.BLOCK);
+        super.tick(state, level, pos, random);
     }
 }
--- src/main/java/com/displace/cavetraps/blockentities/ExplosiveTrapBlockEntity.java
+++ src/main/java/com/displace/cavetraps/blockentities/ExplosiveTrapBlockEntity.java
@@ -1,15 +1,22 @@
 package com.displace.cavetraps.blockentities;
 
+import com.displace.cavetraps.block.ExplosiveTrapBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class ExplosiveTrapBlockEntity extends BlockEntity implements GeoBlockEntity {
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
+    private static final RawAnimation IDLE = RawAnimation.begin().thenLoop("idle");
+
+    private static final RawAnimation ACTIVATED = RawAnimation.begin().thenPlayXTimes("activated", 1).thenLoop("armed");
 
     public ExplosiveTrapBlockEntity(BlockPos pos, BlockState blockState) {
         super(ModBlockEntities.EXPLOSIVE_TRAP_BLOCK_ENTITY.get(), pos, blockState);
@@ -17,12 +24,29 @@ public class ExplosiveTrapBlockEntity extends BlockEntity implements GeoBlockEnt
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(state -> {
+            boolean isTriggered = false;
 
+            // Check the current block state to see if the block has been triggered
+            if (this.getBlockState().hasProperty(ExplosiveTrapBlock.TRIGGERED)) {
+                isTriggered = this.getBlockState().getValue(ExplosiveTrapBlock.TRIGGERED);
+            }
+
+            // Play the activated animation if triggered, otherwise sit idle
+            return state.setAndContinue(isTriggered ? ACTIVATED : IDLE);
+        }));
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.geoCache;
+    }
+
+    public void activate() {
+        setChanged();
+        if (level != null) {
+            level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), 3);
+        }
     }
 
 }
